2b46d68d07abda4f560836b7934bb5ac7886fce3
Adrian Rangel Martinez <142278016+AdrianRang@users.noreply.github.com>
2026-09-18 17:35:18 -0400

    you already know it

--- ControllerCode/src/main.rs
+++ ControllerCode/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, net::UdpSocket, thread::sleep, time::Duration};
+use std::{io::Write, net::UdpSocket, sync::{Arc, RwLock}, thread::{self, sleep}, time::Duration};
 
 use crossterm::event::{self, Event};
 
@@ -8,54 +8,70 @@ fn main() -> std::io::Result<()>{
     crossterm::terminal::enable_raw_mode()?;
     
     let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
-    let mut input = String::new();
+    let input = Arc::new(RwLock::new(String::new()));
+    let input_reciever = Arc::clone(&input);
+
+    let send = Arc::new(RwLock::new(false));
+
+    let input_writer = Arc::clone(&input);
+    let send_writer = Arc::clone(&send);
+    let _input_thread = thread::spawn(move || {
+        loop {
+            if *input_writer.read().unwrap() == "exit" && *send_writer.read().unwrap() { break; }
+            if event::poll(Duration::from_millis(50)).expect("Poll Error") {
+                if let Event::Key(key_event) = event::read().expect("Read Error") {
+                    match key_event.code {
+                        event::KeyCode::Enter => {
+                            if input_writer.read().unwrap().len() == 0 { continue; }
+                            // message = input.clone().leak();
+                            *send_writer.write().unwrap() = true;
+                            // *input_writer.write().unwrap() = String::new();
+                            print!("\r\n");
+                            // break;
+                        },
+                        event::KeyCode::Char(c) => {
+                            input_writer.write().unwrap().push(c);
+                            print!("{}", c);
+                            std::io::stdout().flush().expect("flush fail");
+                        },
+                        event::KeyCode::Backspace => { input_writer.write().unwrap().pop(); print!("\x08 \x08"); std::io::stdout().flush().expect("Error Flushing stdout"); }
+                        event::KeyCode::Esc => { *input_writer.write().unwrap() = "exit".to_owned(); *send_writer.write().unwrap() = true; }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    });
 
     loop {
         // print!("1\n\r");
         let mut message = "ACK";
 
-        loop {
-            if event::poll(Duration::ZERO)? {
-                if let Event::Key(key_event) = event::read()? {
-                    match key_event.code {
-                        event::KeyCode::Enter => {
-                            if input.len() == 0 { continue; }
-                            message = input.clone().leak();
-                            input = String::new();
-                            print!("\r\n");
-                            // break;
-                        },
-                        event::KeyCode::Char(c) => {
-                            input.push(c);
-                            print!("{}", c);
-                            std::io::stdout().flush()?
-                        },
-                        event::KeyCode::Backspace => { input.pop(); print!("\x08 \x08"); std::io::stdout().flush()?; }
-                        event::KeyCode::Esc => { message = "exit"; break; }
-                        _ => {}
-                    }
-                }
-            } else { break; }
+        if *send.read().unwrap() && input_reciever.read().unwrap().len() > 0 {
+            message = input_reciever.read().unwrap().clone().leak();
+            *input_reciever.write().unwrap() = String::new();
+            *send.write().unwrap() = false;
         }
-        // print!("2\n\r");
+                // print!("2\n\r");
         
         if message == "exit" { break; }
 
         socket.send_to(message.as_bytes(), "192.168.4.1:3526")?;
+        if message != "ACK" {print!("sent {}\n\r", message)}
         // println!("SENT MESSAGE {}", message);
         // print!("3\n\r");
     
         let mut buffer = [0; 1024];
 
         let (amt, _) = socket.recv_from(&mut buffer)?;
-        // print!("4\n\r");
 
-        // let _ = str::from_utf8(&buffer[..amt]).unwrap_or("Invalid UTF-8 Data");
+        let recieved = str::from_utf8(&buffer[..amt]).unwrap_or("Invalid UTF-8 Data");
+        if message != "ACK" {print!("recieved {}\n\r", recieved)}
 
         // print!("5\n\r");
         // println!("Recieved reply {}, from {}", response, src);
 
-        // sleep(Duration::from_millis(100));
+        sleep(Duration::from_millis(10));
     }
 
     crossterm::terminal::disable_raw_mode()?;
--- RobotCode/src/main.cpp
+++ RobotCode/src/main.cpp
@@ -1,3 +1,4 @@
+#include "HardwareSerial.h"
 #include "esp32-hal-gpio.h"
 #include "esp32-hal.h"
 #include <Arduino.h>
@@ -6,6 +7,12 @@
 #include <ESP32Servo.h>
 
 #define DISCONNECT_TIME 1000.0 // ms
+#define LEFT 32
+#define RIGHT 33
+#define WEAPON 2
+
+#define DRIVE_VALUE 180
+#define STOP_VALUE 95
 
 const char *ssid = "test";
 const char *password = "password123";
@@ -14,42 +21,100 @@ const char *hostname = "esp.local";
 WiFiUDP udp;
 uint port = 3526;
 
-Servo servo;
+Servo leftMotor;
+Servo rightMotor;
+Servo weaponMotor;
 
-double forewardStart = -1;
-double forewardDuration = 0;
+double backStart = -1;
+double backDuration = 0;
+
+double leftStart = -1;
+double leftDuration = 0;
+
+double rightStart = -1;
+double rightDuration = 0;
+
+double frontStart = -1;
+double frontDuration = 0;
 
 void stopAll() {
-  servo.write(95);
+  leftMotor.write(STOP_VALUE);
+  rightMotor.write(STOP_VALUE);
+  weaponMotor.write(STOP_VALUE);
 
-  forewardStart = -1;
-  forewardDuration = 0;
+  frontStart = -1;
+  frontDuration = 0;
 
   digitalWrite(2, int(millis() / 100.0) % 2 == 0 ? HIGH : LOW);
   // digitalWrite(2, LOW);
 }
 
 void stop() {
-  forewardStart = -1;
-  forewardDuration = 0;
+  frontStart = -1;
+  frontDuration = 0;
+  backStart = -1;
+  backDuration = 0;
+  leftStart = -1;
+  leftDuration = 0;
+  rightStart = -1;
+  rightDuration = 0;
+
+  leftMotor.write(STOP_VALUE);
+  rightMotor.write(STOP_VALUE);
+  weaponMotor.write(STOP_VALUE);
 }
 
 void front() {
+  Serial.println("front");
+  leftMotor.write(DRIVE_VALUE);
+  rightMotor.write(DRIVE_VALUE);
+  
+  backStart = -1;
+  backDuration = 0;
+  leftStart = -1;
+  leftDuration = 0;
+  rightStart = -1;
+  rightDuration = 0;
 }
 
 void back() {
-  forewardStart = -1;
-  forewardDuration = 0;
+  frontStart = -1;
+  frontDuration = 0;
+  leftStart = -1;
+  leftDuration = 0;
+  rightStart = -1;
+  rightDuration = 0;
+
+  leftMotor.write(-DRIVE_VALUE);
+  rightMotor.write(-DRIVE_VALUE);
 }
 
 void left() {
-  forewardStart = -1;
-  forewardDuration = 0;
+  leftMotor.write(-DRIVE_VALUE);
+  rightMotor.write(DRIVE_VALUE);
+
+  
+  frontStart = -1;
+  frontDuration = 0;
+  backStart = -1;
+  backDuration = 0;
+  rightStart = -1;
+  rightDuration = 0;
 }
 
 void right() {
-  forewardStart = -1;
-  forewardDuration = 0;
+  frontStart = -1;
+  frontDuration = 0;
+  leftMotor.write(DRIVE_VALUE);
+  rightMotor.write(-DRIVE_VALUE);
+
+  
+  frontStart = -1;
+  frontDuration = 0;
+  backStart = -1;
+  backDuration = 0;
+  leftStart = -1;
+  leftDuration = 0;
 }
 
 
@@ -63,12 +128,14 @@ void setup() {
   Serial.println("Started Wifi");
   Serial.println(WiFi.softAPIP().toString());
 
+  leftMotor.attach(LEFT);
+  rightMotor.attach(RIGHT);
+  weaponMotor.attach(WEAPON);
+
   pinMode(2, OUTPUT);
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
   digitalWrite(13, HIGH);
-  servo.attach(12);
-
   udp.begin(port);
 }
 
@@ -77,16 +144,20 @@ bool on = true;
 
 double timeSinceLastPacket;
 
-
 void loop() {
   if(millis() - timeSinceLastPacket > DISCONNECT_TIME) {
+    Serial.println("Disconnect");
     stopAll();
+  } else if(frontStart + frontDuration >= millis()) {
+    front();
+  } else if (backStart + backDuration >= millis()) {
+    back();
+  } else if (leftStart + leftDuration >= millis()) {
+    left();
+  } else if (rightStart + rightDuration >= millis()) {
+    right();
   } else {
     digitalWrite(2, active ? LOW : HIGH);
-  }
-
-  if(forewardStart + forewardDuration >= millis()) {
-    front();
   }
 
   int packetSize = udp.parsePacket();
@@ -103,12 +174,29 @@ void loop() {
       active = !active;
       digitalWrite(2, active ? LOW : HIGH);
     } else if (packet.startsWith("front")) {
-      double time = packet.substring(String("front").length()).toDouble();
-      forewardDuration = time == 0 ? 500 : time;
-      forewardStart = millis();
+      double time = packet.substring(String("front").length()).toDouble() * 1000;
+      frontDuration = time == 0 ? 500 : time;
+      frontStart = millis();
+      Serial.printf("Going front for %f seconds\n", frontDuration/1000);
+    } else if (packet.startsWith("back")) {
+      double time = packet.substring(String("back").length()).toDouble() * 1000;
+      backDuration = time == 0 ? 500 : time;
+      backStart = millis();
+      Serial.printf("Going back for %f seconds\n", backDuration/1000);
+    } else if (packet.startsWith("left")) {
+      double time = packet.substring(String("left").length()).toDouble() * 1000;
+      leftDuration = time == 0 ? 500 : time;
+      leftStart = millis();
+      Serial.printf("Going left for %f seconds\n", leftDuration/1000);
+    } else if (packet.startsWith("right")) {
+      double time = packet.substring(String("right").length()).toDouble() * 1000;
+      rightDuration = time == 0 ? 500 : time;
+      rightStart = millis();
+      Serial.printf("Going right for %f seconds\n", rightDuration/1000);
     } else if(packet.startsWith("s")) {
-      servo.write(packet.substring(1).toInt());
-      Serial.println(packet.substring(1).toInt());
+      stop();
+      // servo.write(packet.substring(1).toInt());
+      // Serial.println(packet.substring(1).toInt());
     } else {
       Serial.println("NO INSTRUCTION");
     }
